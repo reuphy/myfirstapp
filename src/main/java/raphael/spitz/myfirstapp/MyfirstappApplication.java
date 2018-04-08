a06f53a3159206653d26f2c35ac740e4376056dd
reuphy <reuphy@gmail.com>
2018-04-08 13:37:02 +0200

    Initial commit

---
+++ src/main/java/raphael/spitz/myfirstapp/MyfirstappApplication.java
@@ -0,0 +1,12 @@
+package raphael.spitz.myfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstappApplication.class, args);
+	}
+}
